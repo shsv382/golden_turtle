3ac3d72d5a3736d5c7f6bd72fda25948e7033b4b
shsv382 <shsv382@gmail.com>
2020-04-13 23:09:55 +0300

    Added some styles to modal window

--- src/components/ImageEdit.jsx
+++ src/components/ImageEdit.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './ImageEdit.scss';
 
-const ImageEdit = ({image}) => {
-	return (<div id="editing-image">
-				<img src={image.urls.regular} />
-			</div>)
+class ImageEdit extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	componentDidMount() {
+		const modal = document.getElementById("editing-image");
+		modal.style.width = document.documentElement.clientWidth - 40 + "px";
+		modal.style.height = document.documentElement.clientHeight/1.5 + "px";
+	}
+
+	render() {
+		const { image } = this.props;
+		return (<div id="editing-image">
+					<img src={image.urls.small} />
+				</div>)
+	}
 }
 
 export default ImageEdit;
